2ae915cfae6c626e1501461d27782296b6a9a646
Josh <jam.millsap@gmail.com>
2018-10-24 23:37:36 -0500

    Add vertical win condition check Add horizontal win condition check

--- Pentago-console/Pentago-console/Program.cs
+++ Pentago-console/Pentago-console/Program.cs
@@ -4,6 +4,46 @@ namespace Pentago_console
 {
     class Program
     {
+        static bool isOver(TileVals[,] board)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int val = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    val += board[i, j];
+                }
+                if (val == 5) { return true; }
+            }
+            for (int i = 1; i < 7; i++)
+            {
+                int val = 0;
+                for (int j = 1; j < 7; j++)
+                {
+                    val += board[i, j];
+                }
+                if (val == 5) { return true; }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                int val = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    val += board[j, i];
+                }
+                if (val == 5) { return true; }
+            }
+            for (int i = 1; i < 7; i++)
+            {
+                int val = 0;
+                for (int j = 1; j < 7; j++)
+                {
+                    val += board[j, i];
+                }
+                if (val == 5) { return true; }
+            }
+            
+        }
         static void Main(string[] args)
         {
             TileVals[,] gameBoard = new TileVals[6, 6];
@@ -15,8 +55,8 @@ namespace Pentago_console
     }
     enum TileVals
     {
-        X = -1,
-        O = 1,
+        X = 1,
+        O = 10,
         Blank = 0
     }
 }
