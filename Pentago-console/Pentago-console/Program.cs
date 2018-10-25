d62f91268c94422257f2bf381e8b62fcac32d790
Connor Goldsmith <cgolds2@users.noreply.github.com>
2018-10-24 23:10:06 -0500

    Created C# console app for testing methods

---
+++ Pentago-console/Pentago-console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pentago_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TileVals[,] gameBoard = new TileVals[6, 6];
+            Console.WriteLine("Enter a number");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered: " + a.ToString());
+            Console.ReadLine();
+        }
+    }
+    enum TileVals
+    {
+        X = -1,
+        O = 1,
+        Blank = 0
+    }
+}
